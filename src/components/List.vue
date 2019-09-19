8e3830b7ff3853d35efcf4aaba7b303e507dbb4b
mccrush <mccrush2027@gmail.com>
2019-09-19 13:40:05 +0700

    Настраивает отображение формы Списка

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="col-5 border-right">
-    <h6 class="pt-3 border-bottom pb-2 font-weight-bold listtitle">
-      {{title}}
+    <h6 v-if="!showForm" class="pt-3 border-bottom pb-2 font-weight-bold listtitle" @click="editItem">
+      {{itemTitle}}
       <button class="btn btn-sm btn-light text-center delbutton" @click="deleteItem">D</button>
     </h6>
-
+    <input class="form-control form-control-sm formitem" v-if="showForm" v-model="itemTitle" @blur="hideForm" @focus="validateDate" type="text" />
     <UlTask :listId="listId" :listObj="listObj" />
   </div>
 </template>
@@ -22,7 +22,10 @@ export default {
     listObj: Object
   },
   data() {
-    return {};
+    return {
+      showForm: false,
+      itemTitle: this.title
+    };
   },
   mounted() {},
   methods: {
@@ -34,6 +37,28 @@ export default {
         projid: this.$store.state.proj,
         listid: this.listId
       });
+    },
+    editItem(e) {
+      this.showForm = true;
+    },
+    validateDate(e) {
+      //e.target.classList.add("border-0");
+      e.target.style = "border-bottom: 1px solid #dee2e6 !important;";
+    },
+    hideForm(e) {
+      if (!this.itemTitle) {
+        //e.target.style = ("border-0");
+        e.target.style = "border-bottom: 1px solid #dc3545 !important;";
+      } else {
+        e.target.style = "border-bottom: 1px solid #dee2e6 !important;";
+        this.showForm = false;
+        this.$store.commit("renameElement", {
+          type: "l",
+          spheid: this.$store.state.sphe,
+          projid: this.$store.state.proj,
+          listid: this.listId
+        });
+      }
     }
   }
 };
@@ -61,5 +86,17 @@ export default {
 
 .listtitle:hover .delbutton {
   display: block;
+}
+
+.formitem {
+  font-size: 16px;
+  font-weight: 700;
+  outline: none;
+  border: none;
+  border-bottom: 1px solid #dee2e6 !important;
+  margin-top: 10px;
+  margin-bottom: 8px;
+  padding: 0 0 1px 0;
+  color: #212529;
 }
 </style>
