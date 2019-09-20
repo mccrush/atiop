2484b767ff6c666cd949122809b31acdb1c7bcb3
mccrush <mccrush2027@gmail.com>
2019-09-20 15:37:54 +0700

    Изменяет внешний вид формы входа

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="container-fluid">
     <div class="row justify-content-center">
-      <div class="col-6 mt-5 border pt-3 pb-3 bg-light">
-        <h3 class="text-center">Login form</h3>
+      <div class="col-4 mt-5 border pt-3 pb-3 bg-light">
+        <h3 class="text-center">Форма входа</h3>
         <form>
           <div class="form-group">
-            <label for="email">Email address</label>
-            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" v-model="email" />
+            <label for="email">Email адрес</label>
+            <input type="email" class="form-control form-control-sm" id="email" aria-describedby="emailHelp" placeholder="Enter email" v-model="email" />
             <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
           </div>
           <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" class="form-control" id="password" placeholder="Password" v-model="password" />
+            <label for="password">Пароль</label>
+            <input type="password" class="form-control form-control-sm" id="password" placeholder="Password" v-model="password" />
           </div>
           <div class="form-group form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            <input type="checkbox" class="form-check-input" id="exampleCheck1" checked />
+            <label class="form-check-label" for="exampleCheck1">Запомнить меня</label>
           </div>
-          <button type="button" @click="login" class="btn btn-secondary btn-block">Submit</button>
+          <button type="button" @click="login" class="btn btn-sm btn-secondary btn-block">Войти</button>
         </form>
       </div>
     </div>
