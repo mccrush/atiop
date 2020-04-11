7a7aed4135f989dc765e4242ea95c136bbc9c9f7
mccrush <mccrush2027@gmail.com>
2020-04-11 20:42:14 +0700

    Настраивает отображение навигации

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="container-fluid">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
+  <div>
+    <Navbar />
+    <div class="container-fluid">
+      <router-view />
     </div>
-    <router-view />
   </div>
 </template>
 
+<script>
+import Navbar from "@/components/Navbar";
+
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
 <style>
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="navbar navbar-expand-sm navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+      <img src="img/icons/favicon-32x32.png" width="30" height="30" class="d-inline-block align-top" alt="ATIOP" />
+      ATIOP
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <router-link to="/" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/about" class="nav-link">About</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "navbar"
+};
+</script>
+
+
+<style scoped>
+</style>
