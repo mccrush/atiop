060c32e940daae9258fca46812295012aa9348f9
mccrush <mccrush2027@gmail.com>
2020-04-11 19:54:39 +0700

    Обнуляет все наработки

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,32 @@
 <template>
   <div id="app">
-    <Head />
-
-    <router-view />
-    <Foot />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import { auth } from "@/main.js";
-import Head from "@/components/Head.vue";
-import Foot from "@/components/Foot.vue";
-
-export default {
-  name: "app",
-  components: {
-    Head,
-    Foot
-  },
-  data() {
-    return {
-      uid: ""
-    };
-  },
-  mounted() {
-    if (auth.currentUser) {
-      this.$store.commit("setUid", {
-        uid: auth.currentUser.uid,
-        uemail: auth.currentUser.email
-      });
-      //this.uid = auth.currentUser.uid;
-    }
-  }
-};
-</script>
-
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,39 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
-let app = "";
-
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore"; // DB
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDVDmMu6JRvBYGGtPG4ZjOT3jp0JRprPL4",
-  authDomain: "atiop-8c255.firebaseapp.com",
-  databaseURL: "https://atiop-8c255.firebaseio.com",
-  projectId: "atiop-8c255",
-  storageBucket: "",
-  messagingSenderId: "880500583039",
-  appId: "1:880500583039:web:c0439e9a7b8c970f"
-};
-firebase.initializeApp(firebaseConfig);
-
-export const fb = firebase.firestore; // Для прямого удаления из БД, используется в файле Store
-export const db = firebase.firestore();
-export const auth = firebase.auth();
-
-firebase.auth().onAuthStateChanged(() => {
-  if (!app) {
-    app = new Vue({
-      router,
-      store,
-      render(h) { return h(App); },
-    }).$mount('#app');
-  }
-});
-// Комментарий
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
