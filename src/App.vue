3cb9a9bbf806fd5ef30f1304c5b28ad843c33df4
mccrush <mccrush2027@gmail.com>
2020-04-11 20:28:05 +0700

    Настраивает общую структуру доски

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
-  <div class="container">
+  <div class="container-fluid">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
-
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,35 @@
 <template>
-  <div class="row">
-    <div class="col-4">
-      <button class="btn btn-success">
-        Button
-        <img src="/assets/img/bootstrap.svg" alt width="32" height="32" title="Bootstrap" />
-      </button>
+  <div class="row border">
+    <div class="col-3 border">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Morbi leo risus</li>
+        <li class="list-group-item">Porta ac consectetur ac</li>
+        <li class="list-group-item">Vestibulum at eros</li>
+      </ul>
+    </div>
+    <div class="col-9 border">
+      <div class="row border">
+        <div class="col-4">
+          <ul class="list-group">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+          </ul>
+        </div>
+        <div class="col-4">
+          <ul class="list-group">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
