0efd8020d000a5c77763e1446b3263fcafd344f6
mccrush <mccrush2027@gmail.com>
2020-04-12 09:24:22 +0700

    Создает компонент проекта

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,21 @@
 <template>
   <div class="row">
-    <div class="col-3 border-right">
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">Dapibus ac facilisis in</li>
-        <li class="list-group-item">Morbi leo risus</li>
-        <li class="list-group-item">Porta ac consectetur ac</li>
-        <li class="list-group-item">Vestibulum at eros</li>
-      </ul>
-    </div>
     <Proj />
+    <List />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 //import HelloWorld from '@/components/HelloWorld.vue'
-import Proj from "@/views/Proj";
+import List from "@/components/Proj";
+import List from "@/components/List";
 
 export default {
   name: "Home",
   components: {
-    Proj
+    Proj,
+    List
   }
 };
 </script>
