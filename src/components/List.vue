0fd9995e4f4599de7018e96b2b08af94b3f945ea
mccrush <mccrush2027@gmail.com>
2020-06-16 20:57:00 +0700

    Fix bug

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col-3 p-1">
+  <div class="col-3 col-lg-2 p-1">
     <h5 class="text-center mt-2">{{type}}</h5>
     <ul v-if="list.length" class="list-group mt-1">
       <li
