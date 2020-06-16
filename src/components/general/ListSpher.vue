886743be85c7d82b5de97b17ad43050a16dff7de
mccrush <mccrush2027@gmail.com>
2020-06-16 20:56:01 +0700

    Add drug scroll

--- src/components/general/ListSpher.vue
+++ src/components/general/ListSpher.vue
@@ -46,6 +46,34 @@ export default {
       type: Array
     }
   },
+  mounted() {
+    const slider = document.querySelector('.ower')
+    let isDown = false
+    let startX
+    let scrollLeft
+
+    slider.addEventListener('mousedown', e => {
+      isDown = true
+      slider.classList.add('active')
+      startX = e.pageX - slider.offsetLeft
+      scrollLeft = slider.scrollLeft
+    })
+    slider.addEventListener('mouseleave', () => {
+      isDown = false
+      slider.classList.remove('active')
+    })
+    slider.addEventListener('mouseup', () => {
+      isDown = false
+      slider.classList.remove('active')
+    })
+    slider.addEventListener('mousemove', e => {
+      if (!isDown) return
+      e.preventDefault()
+      const x = e.pageX - slider.offsetLeft
+      const walk = x - startX
+      slider.scrollLeft = scrollLeft - walk
+    })
+  },
   methods: {
     editItem({ id, type }) {
       this.$emit('edit-item', { id, type })
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -48,6 +48,34 @@ export default {
       return this.projects.filter(proj => proj.childs > 0)
     }
   },
+  mounted() {
+    const slider = document.querySelector('.ower')
+    let isDown = false
+    let startX
+    let scrollLeft
+
+    slider.addEventListener('mousedown', e => {
+      isDown = true
+      slider.classList.add('active')
+      startX = e.pageX - slider.offsetLeft
+      scrollLeft = slider.scrollLeft
+    })
+    slider.addEventListener('mouseleave', () => {
+      isDown = false
+      slider.classList.remove('active')
+    })
+    slider.addEventListener('mouseup', () => {
+      isDown = false
+      slider.classList.remove('active')
+    })
+    slider.addEventListener('mousemove', e => {
+      if (!isDown) return
+      e.preventDefault()
+      const x = e.pageX - slider.offsetLeft
+      const walk = x - startX
+      slider.scrollLeft = scrollLeft - walk
+    })
+  },
   methods: {
     editItem({ id, type }) {
       if (type === 'tasks') {
