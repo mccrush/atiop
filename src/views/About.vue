0f6c67168dcc88144965b434eb78dd75efef7976
mccrush <mccrush2027@gmail.com>
2020-06-26 11:48:00 +0700

    Update Abot page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row shadow-sm">
-    <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4 m-auto h-100">
+    <div class="col-12 col-md-10 col-lg-8 col-xl-6 m-auto h-100">
       <h3 class="mt-4 text-center">ATIOP</h3>
       <p class="text-center text-muted small">
         Версия
@@ -9,140 +9,66 @@
           target="_blank"
           title="Страница на Github"
         >0.2.0-beta</a>
+        <br />По всем вопросам: mccrush2027@gmail.com
       </p>
 
-      <div class="d-flex justify-content-center">
-        <ul class="list-group list-group-horizontal">
-          <li class="list-group-item">
-            <a
-              @click.stop="scrollTo('desc')"
-              href="#desc"
-              class="btn btn-sm btn-light rounded-0"
-            >Описание</a>
-          </li>
-          <li class="list-group-item">
-            <a
-              @click.stop="scrollTo('pages')"
-              href="#pages"
-              class="btn btn-sm btn-light rounded-0"
-            >Страницы</a>
-          </li>
-          <li class="list-group-item">
-            <a
-              @click.stop="scrollTo('props')"
-              href="#props"
-              class="btn btn-sm btn-light rounded-0"
-            >Свойства</a>
-          </li>
-          <li class="list-group-item">
-            <a
-              @click.stop="scrollTo('contacts')"
-              href="#contacts"
-              class="btn btn-sm btn-light rounded-0"
-            >Контакты</a>
-          </li>
-        </ul>
+      <div class="row">
+        <div class="col-12 col-sm-3 col-md-3 mt-2 mb-4">
+          <div class="list-group list-group-flush">
+            <a href="#description" class="list-group-item list-group-item-action p-1 pl-2">Описание</a>
+
+            <a href="#pages" class="list-group-item list-group-item-action p-1 pl-2">Страницы</a>
+
+            <a href="#props" class="list-group-item list-group-item-action p-1 pl-2">Свойства</a>
+          </div>
+        </div>
+        <div class="col-12 col-sm-9 col-md-9">
+          <component :is="component" />
+        </div>
       </div>
 
-      <h5 class="mt-4" ref="desc" :class="{'text-primary': $route.hash === '#desc'}">Описание</h5>
-      <p>
-        ATIOP — сервис управления задачами. Название сервиса — это аббревиатура английских слов
-        <strong>A</strong>ll
-        <strong>T</strong>ask
-        <strong>I</strong>n
-        <strong>O</strong>ne
-        <strong>P</strong>lace (все задачи в одном месте). Одна из основных идей — структура представления задач: направление деятельности &Rightarrow; проект &Rightarrow; задача.
-      </p>
-      <p>
-        Каждый уровень представляет собой ответ на соответствующий вопрос:
-        <br />1. Направление —
-        <strong>Зачем</strong> мы это делаем? Для чего мы вообще решили этим заниматься?
-        <br />2. Проект —
-        <strong>Что</strong> мы хотим получить в итоге? Что будет результатом нашей деятельности?
-        <br />3. Задача —
-        <strong>Как</strong> мы достигнем этого результата? Какие именно действия необходимо выполнить?
-      </p>
-      <p>
-        Для работы с сервисом, необходимо
-        <router-link to="/login#in" tag="a" title="Авторизоваться">авторизоваться</router-link>&nbsp;или
-        <router-link to="/login#reg" tag="a" title="Зарегистрироваться">зарегистрироваться</router-link>.
-      </p>
-
-      <h5
-        class="mt-4"
-        ref="pages"
-        :class="{'text-primary': $route.hash === '#pages'}"
-      >Назначение страниц</h5>
-      <p>На странице &laquo;Проекты&raquo;, представлены списки задач существующих проектов. Можно отфильтровать проекты по направлениям для удобства работы.</p>
-      <p>Страница &laquo;Обзор&raquo; служит для представления иерархической структуры направлений и проектов, что позволяет видеть общую картину всей деятельности.</p>
-      <p>&laquo;Планы&raquo; представляют собой календарь месяцев на ближайший год. Если номер месяца дедлайна (срок сдачи) проекта совпадает с номером представленного месяца, то проект отображается именно в этом месяце.</p>
-      <p>На странице &laquo;Создать&raquo; можно создавать направления, проекты и задачи.</p>
-
-      <h5
-        class="mt-4"
-        ref="props"
-        :class="{'text-primary': $route.hash === '#props'}"
-      >Свойства объектов</h5>
-      <table class="table">
-        <tr>
-          <td></td>
-          <td class="text-center">Направление</td>
-          <td class="text-center">Проект</td>
-          <td class="text-center">Задача</td>
-        </tr>
-        <tr>
-          <td>Заголовок</td>
-          <td class="text-center">+</td>
-          <td class="text-center">+</td>
-          <td class="text-center">+</td>
-        </tr>
-        <tr>
-          <td>Дедлайн</td>
-          <td class="text-center"></td>
-          <td class="text-center">+</td>
-          <td class="text-center">+</td>
-        </tr>
-        <tr>
-          <td>Цвет</td>
-          <td class="text-center"></td>
-          <td class="text-center">+</td>
-          <td class="text-center"></td>
-        </tr>
-      </table>
-
-      <h5
-        class="mt-4"
-        ref="contacts"
-        :class="{'text-primary': $route.hash === '#contacts'}"
-      >Контакты</h5>
-      <p>По всем вопросам и предложениям пишите на почту mccrush2027@gmail.com</p>
-      <button
+      <!-- <button
         class="totop btn btn-light text-center text-muted shadow-sm mb-4 p-0 pl-3 pr-3 pb-1"
         @click.prevent="scrollTop"
       >
         <span>&wedge;</span>
-      </button>
+      </button>-->
     </div>
   </div>
 </template>
 
 <script>
+import Description from '@/views/about/Description'
+import Pages from '@/views/about/Pages'
+import Props from '@/views/about/Props'
+
 export default {
-  methods: {
-    scrollTo(ref) {
-      this.$refs[ref].scrollIntoView({ behavior: 'smooth' })
-    },
-    scrollTop() {
-      window.scrollTo({ top: 0, behavior: 'smooth' })
+  components: {
+    Description,
+    Pages,
+    Props
+  },
+  mounted() {},
+  computed: {
+    component() {
+      return this.$route.hash.split('#')[1] || 'description'
     }
+  },
+  methods: {
+    // scrollTo(ref) {
+    //   this.$refs[ref].scrollIntoView({ behavior: 'smooth' })
+    // },
+    // scrollTop() {
+    //   window.scrollTo({ top: 0, behavior: 'smooth' })
+    // }
   }
 }
 </script>
 
 <style scoped>
-li {
+/* li {
   padding: 0;
-}
+} */
 
 li > a {
   text-decoration: none;
@@ -150,10 +76,9 @@ li > a {
 }
 
 .totop {
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  margin-right: -80px;
+  position: fixed;
+  bottom: 8px;
+  right: 32px;
   font-size: 1.5rem;
   font-weight: bold;
 }
---
+++ src/views/about/Props.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h5>Свойства объектов</h5>
+    <table class="table">
+      <tr>
+        <td></td>
+        <td class="text-center">Направление</td>
+        <td class="text-center">Проект</td>
+        <td class="text-center">Задача</td>
+      </tr>
+      <tr>
+        <td>Заголовок</td>
+        <td class="text-center">+</td>
+        <td class="text-center">+</td>
+        <td class="text-center">+</td>
+      </tr>
+      <tr>
+        <td>Дедлайн</td>
+        <td class="text-center"></td>
+        <td class="text-center">+</td>
+        <td class="text-center">+</td>
+      </tr>
+      <tr>
+        <td>Цвет</td>
+        <td class="text-center"></td>
+        <td class="text-center">+</td>
+        <td class="text-center"></td>
+      </tr>
+    </table>
+  </div>
+</template>
