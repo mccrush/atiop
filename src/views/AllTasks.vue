a66a6a0c949885c27c4efac9b4c2cdd18b21543b
mccrush <mccrush2027@gmail.com>
2020-09-06 20:41:18 +0700

    СCustom styles task

--- src/views/AllTasks.vue
+++ src/views/AllTasks.vue
@@ -1,8 +1,25 @@
-<template></template>
+<template>
+  <div class="d-flex flex-row">
+    <div
+      v-for="task in tasks"
+      :key="task.id"
+      class="task align-self-start border rounded-sm pt-1 pb-1 pl-2 pr-2 m-2"
+    >{{task.title}}</div>
+  </div>
+</template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    tasks() {
+      return this.$store.getters.tasks
+    },
+  },
+}
 </script>
 
 <style scoped>
+.task {
+  width: 256px;
+}
 </style>
