bdfa55e4cb3cfc706b81fa9cac0d863657a0d261
mccrush <mccrush2027@gmail.com>
2020-09-07 14:08:41 +0700

    Create Task component

---
+++ src/components/alltasks/Task.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="task align-self-start border rounded-sm pt-1 pb-1 pl-2 pr-2 m-2"
+    @dblclick.prevent="$emit('edit-item', {id: task.id, type: task.type})"
+  >{{task.title}}</div>
+</template>
+
+<script>
+export default {
+  props: ['task'],
+}
+</script>
+
+<style scoped>
+.task {
+  width: 256px;
+  user-select: none;
+  cursor: default;
+}
+</style>
--- src/views/AllTasks.vue
+++ src/views/AllTasks.vue
@@ -1,25 +1,41 @@
 <template>
   <div class="d-flex flex-row">
-    <div
-      v-for="task in tasks"
-      :key="task.id"
-      class="task align-self-start border rounded-sm pt-1 pb-1 pl-2 pr-2 m-2"
-    >{{task.title}}</div>
+    <Task v-for="task in tasks" :key="task.id" :task="task" @edit-item="editItem" />
+    <Modal :item="item" />
   </div>
 </template>
 
 <script>
+import bootstrap from 'bootstrap/dist/js/bootstrap.min.js'
+import Task from '@/components/alltasks/Task'
+import Modal from '@/components/Modal'
+
 export default {
+  components: {
+    Task,
+    Modal,
+  },
+  data() {
+    return {
+      item: null,
+    }
+  },
   computed: {
     tasks() {
       return this.$store.getters.tasks
     },
   },
+  methods: {
+    editItem({ id, type }) {
+      if (type === 'tasks') {
+        this.item = this.tasks.find((item) => item.id === id)
+      } else {
+        this.item = this.projects.find((item) => item.id === id)
+      }
+      let myModal = new bootstrap.Modal(document.getElementById('exampleModal'))
+      myModal.show()
+    },
+  },
 }
 </script>
 
-<style scoped>
-.task {
-  width: 256px;
-}
-</style>
