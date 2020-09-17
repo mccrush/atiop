ded9f49b585cec7e8153f337e05d73b090ae55df
mccrush <mccrush2027@gmail.com>
2020-09-17 22:10:38 +0700

    Add views for Calendar

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -7,21 +7,42 @@
 
     <div class="col-12">
       <div class="row p-0 border-bottom">
-        <div class="col-1 p-2">
-          <button class="btn btn-sm btn-block btn-light">Месяц</button>
+        <div class="col-3 p-2">
+          <button class="btn btn-sm btn-light rounded-sm float-left w-25">День</button>
+          <input
+            type="datetime-local"
+            id="day"
+            class="form-control form-control-sm rounded-sm w-75"
+            v-model="day"
+          />
+          <!-- <select class="form-control form-control-sm rounded-sm w-50">
+            <option value selected>Выбрать</option>
+            <option value="status">Вторая</option>
+            <option value="napravId">Третяя</option>
+            <option value="napravId">Четвертая</option>
+          </select>-->
         </div>
-        <div class="col-1 p-2">
-          <select class="form-control form-control-sm">
+        <div class="col-2 p-2">
+          <button class="btn btn-sm btn-light rounded-sm float-left w-50">Неделя</button>
+          <select class="form-control form-control-sm rounded-sm w-50">
+            <option value selected>Первая</option>
+            <option value="status">Вторая</option>
+            <option value="napravId">Третяя</option>
+            <option value="napravId">Четвертая</option>
+          </select>
+        </div>
+        <div class="col-3 p-2">
+          <button class="btn btn-sm btn-light float-left w-50">Месяц</button>
+          <select class="form-control form-control-sm w-50">
             <option value selected>Сентябрь</option>
             <option value="status">Октябрь</option>
             <option value="napravId">Ноябрь</option>
           </select>
         </div>
-        <div class="col-1 p-2">
-          <button class="btn btn-sm btn-block btn-light">Год</button>
-        </div>
-        <div class="col-1 p-2">
-          <select class="form-control form-control-sm">
+        <div class="col-2 p-2">
+          <!-- Активный способ представления с жетой рамкой -->
+          <button class="btn btn-sm btn-light border-warning float-left w-50">Год</button>
+          <select class="form-control form-control-sm w-50">
             <option value selected>2017</option>
             <option value="status">2018</option>
             <option value="napravId">2019</option>
