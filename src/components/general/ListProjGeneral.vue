bf4c45cd024ecdc0ebd0083a275ef0a3d546ae8c
mccrush <mccrush2027@gmail.com>
2020-09-20 20:08:09 +0700

    Add show task list in Napravs

--- src/components/general/ListProjGeneral.vue
+++ src/components/general/ListProjGeneral.vue
@@ -2,8 +2,8 @@
   <div class="col-2">
     <ul v-if="displayProj.length" class="list-group list-group-horizontal mt-2">
       <li
-        v-for="(item, index) in displayProj"
-        :key="'in'+index"
+        v-for="item in displayProj"
+        :key="item.id"
         class="list-group-item justify-content-between align-items-center border-0 p-0 ml-1 mr-1 cursor-pointer"
       >
         <h6
@@ -11,6 +11,7 @@
           :style="{'background': item.color ? item.color+'!important' : '#f8f9fa!important'}"
           @dblclick.prevent="$emit('edit-item', {id: item.id, type: item.type})"
         >{{item.title}}</h6>
+        <TaskList :projectId="item.id" />
       </li>
     </ul>
     <!-- <p v-else class="li">
@@ -20,7 +21,12 @@
 </template>
 
 <script>
+import TaskList from '@/components/general/TaskList'
+
 export default {
+  components: {
+    TaskList,
+  },
   props: ['projects', 'napravId', 'color'],
   computed: {
     displayProj() {
---
+++ src/components/general/TaskList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="task-list">
+    <Task
+      v-for="task in taskProjectId"
+      :key="task.id"
+      :task="task"
+      @edit-item="editItem"
+      :settings="settings"
+    />
+  </div>
+</template>
+
+<script>
+import Task from '@/components/alltasks/Task'
+
+export default {
+  components: {
+    Task,
+  },
+  props: ['projectId'],
+  computed: {
+    tasks() {
+      return this.$store.getters.tasks
+    },
+    taskProjectId() {
+      return this.tasks.filter((task) => task.projectId === this.projectId)
+    },
+    settings() {
+      return this.$store.getters.settings
+    },
+  },
+}
+</script>
