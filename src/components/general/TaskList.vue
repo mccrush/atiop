0e234efee415a4b88ba1b1883bac3ed73b593fb2
mccrush <mccrush2027@gmail.com>
2020-09-24 18:50:47 +0700

    Add margin right

--- src/components/general/TaskList.vue
+++ src/components/general/TaskList.vue
@@ -11,7 +11,7 @@
       v-if="status !== 'done'"
       type="text"
       class="form-control form-control-sm border-0 bg-light ml-2 mb-2 add-task"
-      :class="{ 'border-danger': error, 'mt-2': !sortTasks.length }"
+      :class="{ 'border-danger': error, 'mt-2 mr-2': !sortTasks.length }"
       placeholder="Добавить задачу"
       v-model.trim="title"
       @focus="error = false"
