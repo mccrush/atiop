b2e803d95e3f0cf5d10630fea8c88aac80a6a325
mccrush <mccrush2027@gmail.com>
2021-02-11 22:13:20 +0700

    Update item props

--- src/scripts/createItem.js
+++ src/scripts/createItem.js
@@ -1,14 +1,16 @@
 import getDateNow from './getDateNow'
 export default (id = Date.now().toString(), title, type = 'tasks', napravId = '', projectId = '', listId = '', length = 0, napravTitle = '', projectTitle = '', listTitle = '') => {
   const item = {
+    id,
     title,
     desc: '',
-    id,
     type,
     napravId,
     napravTitle,
     projectId,
     projectTitle,
+    listId,
+    listTitle,
     status: 'todo',
     position: length + 1, // По умолчанию в конец списка
     color: '', // У задач нет цвета
