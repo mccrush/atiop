e26cd52957d4ffe0de3b1d0af5daa0e7c978f91e
mccrush <mccrush2027@gmail.com>
2021-02-17 14:58:32 +0700

    Update icns settings

--- vue.config.js
+++ vue.config.js
@@ -5,8 +5,16 @@ module.exports = {
   pwa: {
     name: 'ATIOP',
     themeColor: '#FFC107',
+    msTileColor: '#FFC107',
     manifestOptions: {
-      background_color: '#FFC107'
+      background_color: '#FFC107',
+    },
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/mstile-150x150.png'
     }
   }
 }
