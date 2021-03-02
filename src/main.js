3a3e856acb5d92d56e27aa5ec0b6d76696630b32
mccrush <mccrush2027@gmail.com>
2021-03-02 14:54:08 +0700

    Remove router

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-import router from './router'
+//import router from './router'
 import store from './store'
 import { auth } from './firebase'
 
@@ -10,7 +10,7 @@ let app
 
 auth.onAuthStateChanged((user) => {
   if (!app) {
-    app = createApp(App).use(store).use(router).mount('#app')
+    app = createApp(App).use(store).mount('#app')
   }
   if (user) {
     store.dispatch('getItems', 'napravs')
