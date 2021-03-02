67749cd9bdf929b39256a73a163276866076fc76
mccrush <mccrush2027@gmail.com>
2021-03-02 20:59:06 +0700

    Create Checklist component

--- src/views2/Checklist.vue
+++ src/views2/Checklist.vue
@@ -1,6 +1,52 @@
 <template>
-  <div class="checklist bg-info p-3">
-    <h2 class="border">Checklist</h2>
+  <div class="row align-content-start bg-info p-3 checklist">
+    <ul class="list-group ms-2">
+      <li class="list-group-item">
+        <input
+          class="form-check-input me-1"
+          type="checkbox"
+          value=""
+          aria-label="..."
+        />
+        First checkbox
+      </li>
+      <li class="list-group-item">
+        <input
+          class="form-check-input me-1"
+          type="checkbox"
+          value=""
+          aria-label="..."
+        />
+        Second checkbox
+      </li>
+      <li class="list-group-item">
+        <input
+          class="form-check-input me-1"
+          type="checkbox"
+          value=""
+          aria-label="..."
+        />
+        Third checkbox
+      </li>
+      <li class="list-group-item">
+        <input
+          class="form-check-input me-1"
+          type="checkbox"
+          value=""
+          aria-label="..."
+        />
+        Fourth checkbox
+      </li>
+      <li class="list-group-item">
+        <input
+          class="form-check-input me-1"
+          type="checkbox"
+          value=""
+          aria-label="..."
+        />
+        Fifth checkbox
+      </li>
+    </ul>
   </div>
 </template>
 
