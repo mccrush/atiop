b7de50197ee6d6ac0916bde0d0ef7a63e0a04a87
mccrush <mccrush2027@gmail.com>
2021-03-07 15:47:37 +0700

    Add tsk Filter for Cards

--- src/views2/Cards.vue
+++ src/views2/Cards.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row d-flex align-content-start bg-light pt-3 ps-2 cards">
     <Item
-      v-for="item in items"
+      v-for="item in tasksFilter"
       :key="'item' + item.id"
       :item="item"
       @edit-item="editItem"
@@ -20,8 +20,22 @@ export default {
     viewType() {
       return this.$store.getters.viewType
     },
-    items() {
-      return this.$store.getters[this.viewType + '2']
+    napravId() {
+      return this.$store.getters.napravId
+    },
+    projectId() {
+      return this.$store.getters.projectId
+    },
+    tasks() {
+      return this.$store.getters.tasks2
+    },
+    tasksFilter() {
+      if (this.projectId) {
+        return this.tasks.filter(item => item.projectId === this.projectId)
+      } else if (this.napravId) {
+        return this.tasks.filter(item => item.napravId === this.napravId)
+      }
+      return this.tasks
     }
   },
   methods: {
