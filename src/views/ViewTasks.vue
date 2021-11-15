72654d828f319f5ed0f91f1f67c6b37b855ce78d
mccrush <mccrush2027@gmail.com>
2021-11-15 18:09:38 +0700

    Update settings name

--- src/views/ViewTasks.vue
+++ src/views/ViewTasks.vue
@@ -35,7 +35,7 @@ export default {
   },
   computed: {
     settings() {
-      return this.$store.getters.settings
+      return this.$store.getters.settingsObj
     },
     filterId() {
       return this.$store.getters.filterId
