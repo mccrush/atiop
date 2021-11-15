667d02a5e90ecbc84c1a3fa1b1e6df67dabd38f7
mccrush <mccrush2027@gmail.com>
2021-11-15 19:24:29 +0700

    Settings Select Year

--- src/views/ViewCalendar.vue
+++ src/views/ViewCalendar.vue
@@ -1,10 +1,40 @@
 <template>
-  <div class="calendar row bg-warning">Calendar</div>
+  <div class="calendar row bg-warning">
+    <div
+      class="
+        select-pole
+        col-12
+        bg-white
+        d-flex
+        justify-content-center
+        pt-2
+        pb-3
+      "
+    >
+      <SelectYear />
+    </div>
+    <div class="col-12">Dates</div>
+  </div>
 </template>
+
+<script>
+import SelectYear from '@/components/calendar/SelectYear'
+
+export default {
+  components: {
+    SelectYear
+  }
+}
+</script>
+
 
 <style scoped>
 .calendar {
   height: calc(100vh - 59px);
+}
+
+.select-pole {
+  height: 56px;
 }
 </style>
 
