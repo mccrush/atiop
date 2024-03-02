03a24de002ea68e2d3e1966aa51b67a66579e64c
mccrush <mccrush2027@gmail.com>
2024-03-02 12:13:06 +0700

    Run propject

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import apiKey from './../apiKey'
+import { apiKey } from './../apiKey'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
