610b4ccebbd17c89ce2663ec517e4e9b41ca0a39
mccrush <mccrush2027@gmail.com>
2024-06-26 10:02:36 +0700

    Update style navigation buttons

--- src/components/buttons/BtnNavigation.vue
+++ src/components/buttons/BtnNavigation.vue
@@ -1,5 +1,9 @@
 <template>
-  <button class="btn btn-light text-body-secondary lh-1">{{ title }}</button>
+  <button
+    class="btn btn-sm btn-dark text-body-secondary lh-1 ps-3 pe-3 pt-2 pb-2"
+  >
+    {{ title }}
+  </button>
 </template>
 
 <script>
